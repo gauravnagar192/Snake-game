2be2a4d841a7d4f01588c20a27e9a3350c5ce244
Administrator <Administrator@ADMIN-PC>
2017-12-20 11:58:19 +0530

    Small but interesting snake game

---
+++ src/snake/game/SnakeGame.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake.game;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Administrator
+ */
+public class SnakeGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame obj = new JFrame();
+        Gameplay gameplay = new Gameplay();
+        
+        obj.setBounds(10,10,905,700);
+        obj.setBackground(Color.darkGray);
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+    
+}
